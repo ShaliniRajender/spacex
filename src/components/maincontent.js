c07e70ad83d21dd4a35b808328e5912e1f826bf7
ShaliniRajender <96040231+ShaliniRajender@users.noreply.github.com>
2023-09-26 14:40:01 +0530

    Update maincontent.js

--- src/components/maincontent.js
+++ src/components/maincontent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Table from './table';
 import { Button } from '@mui/material';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
 const maincontent = () => {
 
     const onredirectlink = () => {
